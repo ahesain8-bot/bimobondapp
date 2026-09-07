6b944e0a80dbf078f8784a2a8f0ab3fce34e6e5a
basharagb <basharagb@gmail.com>
2026-09-07 16:02:36 +0300

    enhacenments

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:bimobondapp/app/live_promotions/presentation/di/live_promotions_injector.dart'
-    as live_promotions_di;
+import 'package:bimobondapp/app/live_promotions/presentation/di/live_promotions_injector.dart' as live_promotions_di;
 import 'package:bimobondapp/core/routes/app_router.dart';
 import 'package:bimobondapp/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
